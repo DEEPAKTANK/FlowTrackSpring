ec306dae94f71333a90f7b1cb146a872ef236f1b
Deepak Tank <deepaktank404@gmail.com>
2025-03-01 15:16:48 +0530

    initial commit

---
+++ src/test/java/com/deepak/proexpenditure/pro_expenditure/ProExpenditureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.proexpenditure.pro_expenditure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProExpenditureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
